3d891975768112d665f32b79c8e18412427c29ad
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-12-11 23:28:57 +0100

    Hello get-by-class

---
+++ Readme.md
@@ -0,0 +1,38 @@
+![web component logo](http://i49.tinypic.com/e7nj9v.png)
+
+# getByClass
+
+A cross-browser implementation of getElementsByClass.
+Heavily based on [Dustin Diaz](https://github.com/ded)'s function: [http://dustindiaz.com/getelementsbyclass](http://www.dustindiaz.com/getelementsbyclass).
+
+Uses default `getElementsByClassName` if it's available.
+
+## Installation
+
+    $ component install javve/get-by-class
+
+## Example
+
+```js
+var getByClass = require('get-by-class');
+
+var menu = document.getElementById('menu');
+
+// Returns set of items
+var activeItem = getByClass('active', menu);
+
+// Returns single item
+var activeItem = getByClass('active', menu, true);
+```
+
+## API
+
+### getByClass(className, container, single)
+
+Find all elements with class `className` inside `container`.  
+Use `single = true` to increase performance in older browsers
+when only one element is needed.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * Find all elements with class `className` inside `container`.
+ * Use `single = true` to increase performance in older browsers
+ * when only one element is needed.
+ *
+ * @param {String} className
+ * @param {Element} container
+ * @param {Boolean} single
+ * @api public
+ */
+
+module.exports = (function() {
+  if (document.getElementsByClassName) {
+    return function(className, container, single) {
+      if (single) {
+        return container.getElementsByClassName(className)[0];
+      } else {
+        return container.getElementsByClassName(className);
+      }
+    };
+  } else {
+    return function(className, container, single) {
+      var classElements = [],
+        tag = '*';
+      if (container == null) {
+        container = document;
+      }
+      var els = container.getElementsByTagName(tag);
+      var elsLen = els.length;
+      var pattern = new RegExp("(^|\\s)"+className+"(\\s|$)");
+      for (var i = 0, j = 0; i < elsLen; i++) {
+        if ( pattern.test(els[i].className) ) {
+          if (single) {
+            return els[i];
+          } else {
+            classElements[j] = els[i];
+            j++;
+          }
+        }
+      }
+      return classElements;
+    };
+  }
+})();
